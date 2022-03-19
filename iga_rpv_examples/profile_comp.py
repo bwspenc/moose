25de3c460eb8f7be780d3d07e5a4f194a5c22d2e
Ben Spencer <benjamin.spencer@inl.gov>
2022-03-18 18:02:14 -0600

    Add 2D Cartesian RPV model

--- iga_rpv_examples/profile_comp.py
+++ iga_rpv_examples/profile_comp.py
@@ -7,13 +7,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-cases=['1d', '1d_coarse', '3d_lagrange', '3d_berenstein']
+cases=['1d', '1d_coarse', '3d_lagrange', '3d_berenstein', '2d_lagrange']
 
 
 names={'3d_berenstein': '3D 2nd order Berenstein, coarse',
         '1d': '1D 2nd order Lagrange, fine',
         '1d_coarse': '1D 2nd order Lagrange, coarse',
-        '3d_lagrange': '3D 2nd order Lagrange, coarse'}
+        '3d_lagrange': '3D 2nd order Lagrange, coarse',
+        '2d_lagrange': '2D Cartesian, 2nd order Lagrange, fine'}
 
 dataframes={}
 for case in cases:
