adae2925b425d6be0ae0883906244d05f0dc5677
Ben Spencer <benjamin.spencer@inl.gov>
2025-03-19 10:00:46 -0600

    Initial template for EHM stress calculator

---
+++ modules/solid_mechanics/include/materials/ComputeEHMStress.h
@@ -0,0 +1,66 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ComputeStressBase.h"
+#include "GuaranteeConsumer.h"
+
+/**
+ * ComputeEHMStress computes the stress following elasticity
+ * theory for finite strains
+ */
+class ComputeEHMStress : public ComputeStressBase, public GuaranteeConsumer
+{
+public:
+  static InputParameters validParams();
+
+  ComputeEHMStress(const InputParameters & parameters);
+
+  void initialSetup() override;
+  virtual void initQpStatefulProperties() override;
+
+protected:
+  virtual void computeQpStress() override;
+
+  /// Name of the elasticity tensor material property
+  const std::string _elasticity_tensor_name;
+  /// Elasticity tensor material property
+  const MaterialProperty<RankFourTensor> & _elasticity_tensor;
+  /// Rotation up to current step "n" to compute anisotropic elasticity tensor
+  MaterialProperty<RankTwoTensor> & _rotation_total;
+  /// Rotation up to "n - 1" (previous) step to compute anisotropic elasticity tensor
+  const MaterialProperty<RankTwoTensor> & _rotation_total_old;
+  /// Strain increment material property
+  const MaterialProperty<RankTwoTensor> & _strain_increment;
+  /// Rotation increment material property
+  const MaterialProperty<RankTwoTensor> & _rotation_increment;
+  /// Old state of the stress tensor material property
+  const MaterialProperty<RankTwoTensor> & _stress_old;
+
+  /**
+   * The old elastic strain is used to calculate the old stress in the case
+   * of variable elasticity tensors
+   */
+  const MaterialProperty<RankTwoTensor> & _elastic_strain_old;
+
+
+  std::vector<RankFourTensor> _coefficient_data;
+
+  /// Number of components
+  const unsigned int _n_parts;
+  /// Number of slip systems
+  const unsigned int _n_slip_sys;
+  /// Vector of initial slip resistances provided by the user
+  const std::vector<Real> _initial_slip_resistance;
+  /// Material property for current slip resistances
+  MaterialProperty<std::vector<std::vector>> & _slip_resistance;
+  /// Old state of material property for current slip resistances
+  const MaterialProperty<std::vector<std::vector>> & _slip_resistance_old;
+};
---
+++ modules/solid_mechanics/src/materials/ComputeEHMStress.C
@@ -0,0 +1,124 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ComputeEHMStress.h"
+
+registerMooseObject("SolidMechanicsApp", ComputeEHMStress);
+
+InputParameters
+ComputeEHMStress::validParams()
+{
+  InputParameters params = ComputeStressBase::validParams();
+  params.addRequiredParam<std::vector<Real>>("initial_slip_resistance",
+                                             "Vector of initial resistance of the components of the slip system");
+  params.addRequiredParam<unsigned int>("num_parts", "Number of parts");
+  params.addRequiredParam<unsigned int>("num_slip_systems", "Number of slip systems");
+  params.addClassDescription("Compute stress using EHM"); //TODO expand on this
+  return params;
+}
+
+ComputeEHMStress::ComputeEHMStress(
+    const InputParameters & parameters)
+  : ComputeStressBase(parameters),
+    GuaranteeConsumer(this),
+    _elasticity_tensor_name(_base_name + "elasticity_tensor"),
+    _elasticity_tensor(getMaterialPropertyByName<RankFourTensor>(_elasticity_tensor_name)),
+    _rotation_total(declareProperty<RankTwoTensor>(_base_name + "rotation_total")),
+    _rotation_total_old(getMaterialPropertyOldByName<RankTwoTensor>(_base_name + "rotation_total")),
+    _strain_increment(getMaterialPropertyByName<RankTwoTensor>(_base_name + "strain_increment")),
+    _rotation_increment(
+        getMaterialPropertyByName<RankTwoTensor>(_base_name + "rotation_increment")),
+    _stress_old(getMaterialPropertyOldByName<RankTwoTensor>(_base_name + "stress")),
+    _elastic_strain_old(getMaterialPropertyOldByName<RankTwoTensor>(_base_name + "elastic_strain")),
+
+    _n_parts(getParam<unsigned int>("num_parts")),
+    _n_slip_sys(getParam<unsigned int>("num_slip_systems")),
+    _initial_slip_reisistance(getParam<std:vector<Real>>("slip_resistance")),
+    _slip_resistance(declareProperty<std::vector<std::vector>>(_base_name + "slip_resistance")),
+    _slip_resistance_old(getMaterialPropertyOldByName<std::vector<std::vector>>(_base_name + "slip_resistance")),
+{
+
+  //TODO error check to make sure _initial_slip_resistance is the right size
+
+  //TODO load data file
+  for (unsigned int i = 0; i< num_parts; ++i)
+  {
+    _coefficient_data[i]= some_rank_four_tensor;
+  }
+
+
+}
+
+void
+ComputeEHMStress::initialSetup()
+{
+}
+
+void
+ComputeEHMStress::initQpStatefulProperties()
+{
+  ComputeStressBase::initQpStatefulProperties();
+  RankTwoTensor identity_rotation(RankTwoTensor::initIdentity);
+
+  _rotation_total[_qp] = identity_rotation;
+
+  //TODO Note that _initial_slip_resistance is a vector of vectors, so need to reshape
+  _slip_resistance[_qp].resize(_n_parts*12);
+  _slip_resistance[_qp] = _initial_slip_resistance;
+}
+
+void
+ComputeEHMStress::computeQpStress()
+{
+  // Calculate the stress in the intermediate configuration
+  RankTwoTensor intermediate_stress;
+
+  if (hasGuaranteedMaterialProperty(_elasticity_tensor_name, Guarantee::ISOTROPIC))
+  {
+    //TODO: Initialize the current slip resistance. You could copy it in like this:
+    _slip_resistance[_qp] = _slip_resistance_old[_qp];
+    //or resize and initialize it:
+    _slip_resistance[_qp].resize(_n_parts*12);
+
+
+    intermediate_stress =
+        _elasticity_tensor[_qp] * (_elastic_strain_old[_qp] + _strain_increment[_qp]);
+
+
+
+
+    // Compute dstress_dstrain
+    _Jacobian_mult[_qp] = _elasticity_tensor[_qp]; // This is NOT the exact jacobian
+  }
+  else
+  {
+    // Rotate elasticity tensor to the intermediate configuration
+    // That is, elasticity tensor is defined in the previous time step
+    // This is consistent with the definition of strain increment
+    // The stress is projected onto the current configuration a few lines below
+    RankFourTensor elasticity_tensor_rotated = _elasticity_tensor[_qp];
+    elasticity_tensor_rotated.rotate(_rotation_total_old[_qp]);
+
+    intermediate_stress =
+        elasticity_tensor_rotated * (_elastic_strain_old[_qp] + _strain_increment[_qp]);
+
+    // Update current total rotation matrix to be used in next step
+    _rotation_total[_qp] = _rotation_increment[_qp] * _rotation_total_old[_qp];
+
+    // Compute dstress_dstrain
+    _Jacobian_mult[_qp] = elasticity_tensor_rotated; // This is NOT the exact jacobian
+  }
+
+  // Rotate the stress state to the current configuration
+  _stress[_qp] =
+      _rotation_increment[_qp] * intermediate_stress * _rotation_increment[_qp].transpose();
+
+  // Assign value for elastic strain, which is equal to the mechanical strain
+  _elastic_strain[_qp] = _mechanical_strain[_qp];
+}
